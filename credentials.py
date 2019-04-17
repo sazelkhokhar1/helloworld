84e137df370fc7c6054873184bbe9cf3c43d3117
sazelkhokhar1 <49514198+sazelkhokhar1@users.noreply.github.com>
2019-04-17 15:45:48 +0200

    Add files via upload

--- credentials.py
+++ credentials.py
@@ -1,25 +1,24 @@
 import mysql.connector
-
-
+import os
 
 mydb = mysql.connector.connect(
   host="localhost",
-  #port="3306",
+  port="3306",
   user="root",
-  passwd="1234",
-  #auth_plugin="mysql_native_password"
+  passwd="1234"
 )
-
 
 
 print(mydb)
 
-
+SQL2 = "CREATE TABLE sys.customers (name VARCHAR(255), id VARCHAR(255))"
+SQL3 = "INSERT INTO sys.customers VALUES (%s, %s)"
+val = ('1','abc')
+SQL1 = "SELECT * FROM sys.customers"
 cursor = mydb.cursor()
-cursor.execute("CREATE TABLE customers (name VARCHAR(255), id VARCHAR(255))")
-SQL = "SELECT * FROM sys.customers"
-cursor.execute(SQL)
-records = cursor.fetchall()
+#cursor.execute(SQL1)
+cursor.execute(SQL3,val)
+mydb.commit()
+#records = cursor.fetchall()
 
 
-#df = pd.DataFrame(records)
